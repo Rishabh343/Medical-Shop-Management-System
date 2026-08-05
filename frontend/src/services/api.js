b17cdbeba2d6fd7e862d934df8149d8453325022
Rishabh343 <sinharishabhkumar8@gmail.com>
2026-08-05 21:03:44 +0530

    fix:baseUrl

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://medical-shop-management-system.onrender.com/api/",
+  baseURL: "https://medical-shop-management-system.onrender.com/api",
   withCredentials: true,
 });
 //
