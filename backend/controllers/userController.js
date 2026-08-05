2082af1904df222a79986fbbea874e20488478f5
Rishabh343 <sinharishabhkumar8@gmail.com>
2026-08-05 21:24:59 +0530

    fix:token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,7 +75,8 @@ export const login = async (req, res) => {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://medical-shop-management-system.onrender.com/api",
   withCredentials: true,
+  
 });
 //
 export default api;
